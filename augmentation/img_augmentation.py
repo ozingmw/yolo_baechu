f52d5d20534e542771e008b5b5099e05d347c3e9
ozingmw <ozingmw@naver.com>
2024-02-29 15:52:43 +0900

    refactoring

--- augmentation/img_augmentation.py
+++ augmentation/img_augmentation.py
@@ -1,29 +1,40 @@
+import argparse
 from glob import glob
-import utils
 import shutil
+from tqdm import tqdm
 
-shutil.rmtree('./datasets', ignore_errors=True)
-shutil.copytree('./baechu_datasets', './datasets')
+import utils
 
-datasets_path = "./datasets"
-train_dataset_path = f"{datasets_path}/train"
-val_dataset_path = f"{datasets_path}/valid"
-test_dataset_path = f"{datasets_path}/test"
+def prepare():
+    shutil.rmtree('./datasets', ignore_errors=True)
+    shutil.copytree('./baechu_datasets', './datasets')
 
-data_aug_count = 32
-
-utils.convert_to_grayscale(train_dataset_path)
-utils.convert_to_grayscale(val_dataset_path)
-utils.convert_to_grayscale(test_dataset_path)
-
-train_images_path_list = [train_dataset for train_dataset in glob(f'{train_dataset_path}/images/*')]
-for train_image in train_images_path_list:
-    utils.augment_image(train_image, data_aug_count)
+def parse_args():
+    parser = argparse.ArgumentParser()
     
-val_images_path_list = [val_dataset for val_dataset in glob(f'{val_dataset_path}/images/*')]
-for val_image in val_images_path_list:
-    utils.augment_image(val_image, data_aug_count)
+    parser.add_argument("--datasets_path", default="./datasets")
+    parser.add_argument("--data_aug_count", default=32)
+    parser.add_argument("--modes", nargs="+", default=["train", "test", "valid"])
+   
+    args = parser.parse_args()
+    
+    return args
 
-test_images_path_list = [test_dataset for test_dataset in glob(f'{test_dataset_path}/images/*')]
-for test_image in test_images_path_list:
-    utils.augment_image(test_image, data_aug_count)
+
+def main(args):
+    datasets_path = args.datasets_path
+    data_aug_count = args.data_aug_count
+    
+    for arg in tqdm(args.modes):
+        path = f"{datasets_path}/{arg}"
+        utils.convert_to_grayscale(path)
+        
+        path_list = [dataset for dataset in glob(f'{path}/images/*')]
+        for image in path_list:
+            utils.augment_image(image, data_aug_count)
+            
+
+if __name__ == "__main__":
+    prepare()
+    args = parse_args()
+    main(args)
